dc19f1c40c24950e388b2def8962edf018f8b215
Joshua Zelen <jzelen@gmail.com>
2018-02-28 01:31:39 -0700

    Created RecyclerView
    1. Custom row layout
    
    2. Button to add new row
    
    3. EditText field for custom message
    
    4. Button to deliver notification with text from EditText field

---
+++ app/src/main/java/com/joshuazelen/myapplication/CustomAdapter.java
@@ -0,0 +1,10 @@
+package com.joshuazelen.myapplication;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Joshua on 2/24/18.
+ */
+
+public class CustomAdapter extends ArrayAdapter {
+}
---
+++ app/src/main/java/com/joshuazelen/myapplication/myRecyclerView.java
@@ -0,0 +1,8 @@
+package com.joshuazelen.myapplication;
+
+/**
+ * Created by Joshua on 2/27/18.
+ */
+
+public class myRecyclerView {
+}
